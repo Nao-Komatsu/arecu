93fe8dfceaae23e2efb675da925d4f04560ea618
Nao Komatsu <riiiin0806@gmail.com>
2019-01-04 14:19:31 +0900

    fix: Decompile command bugs

--- arecu.py
+++ arecu.py
@@ -17,7 +17,7 @@ import configparser
 import modules
 from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
 
-VERSION = '1.6.1'
+VERSION = '2.0.0'
 
 
 ##### Make Parser #####
@@ -25,9 +25,9 @@ VERSION = '1.6.1'
 # Top-level parser
 parser = argparse.ArgumentParser(
         prog = 'Arecu',
-        usage = 'arecu [options...] <apk_file>',
+        usage = 'arecu {subcommand} [options...] <target>',
         description = '%(prog)s is reverse engineering tool for apk.',
-        epilog = 'Copyright (C) 2018 Nao Komatsu',
+        epilog = 'Copyright (C) 2018 - 2019 Nao Komatsu',
         add_help = True)
 
 parser.add_argument('--version',
@@ -35,71 +35,67 @@ parser.add_argument('--version',
         action = 'version',
         default = False)
 
-subparsers = parser.add_subparsers()
+parser.set_defaults(func=modules.function.help)
+
+# Sub-commands parser
+subparsers = parser.add_subparsers(
+        title = 'subcommands',
+        prog = 'arecu',
+        help = 'For more detailed help add --help')
 
 # "dec" command parser
-parser_dec = subparsers.add_parser('dec')
+parser_dec = subparsers.add_parser('decompile',
+        aliases = ['dec'],
+        help = 'Decompile & Decode the apk file')
+
 parser_dec.set_defaults(func=modules.decompile.main)
 
 parser_dec.add_argument('apk_file',
-        help = 'Target apk file.')
+        help = 'Target apk file')
 
 parser_dec.add_argument('-A', '--all',
-        help = 'Unzip, Decompile and Decode with one option.',
+        help = 'Unzip, Decompile and Decode with one option',
         action = 'store_true',
         default = False)
 
 parser_dec.add_argument('-u', '--unzip',
-        help = 'Unzip the apk file.',
+        help = 'Unzip the apk file',
         action = 'store_true',
         default = False)
 
 parser_dec.add_argument('-j', '--jdcmd',
-        help = 'Decompile the apk file using JavaDecompiler.',
+        help = 'Decompile the apk file using JavaDecompiler',
         action = 'store_true',
         default = False)
 
 parser_dec.add_argument('-p', '--procyon',
-        help = 'Decompile the apk file using Procyon Decompiler.',
+        help = 'Decompile the apk file using Procyon Decompiler',
         action = 'store_true',
         default = False)
 
 parser_dec.add_argument('-a', '--apktool',
-        help = 'Decode the apk file using Apktool.',
+        help = 'Decode the apk file using Apktool',
         action = 'store_true',
         default = False)
 
 parser_dec.add_argument('-o', '--outdir',
-        help = 'The name of directory that gets written. Default is current directory.',
+        help = 'The name of directory that gets written. Default is current directory',
         type = str,
         default = '.')
 
 parser_dec.add_argument('-v', '--verbose',
-        help = 'Increase verbosity level.',
+        help = 'Increase verbosity level',
         action = 'store_true',
         default = False)
 
 
-##### Configuration #####
+##### Main Process #####
+
+# Logging
+logger = getLogger('arecu')
 
 # Argument Analysis
 args = parser.parse_args()
 
-# Logging
-if (args.verbose):
-    level = 'DEBUG'
-else:
-    level = 'INFO'
-
-logger = getLogger('arecu')
-modules.log.config(level)
-
-
-##### Main Process #####
-
-def main():
-    args.func(args, level)
-
-
-if __name__ == '__main__':
-    main()
+# -> Sub Process
+args.func(args)
